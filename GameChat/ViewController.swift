238d82b3997d4e7ed1102e50aac9fc8968433a09
Saikrishna Bonagiri <saikrishna.bonagiri@gmail.com>
2017-01-17 01:35:49 -0500

    second commmit

--- GameChat/ViewController.swift
+++ GameChat/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(handlelogout))
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func handlelogout(){
+        let loginController = LoginController()
+        present(loginController, animated: true, completion: nil)
+        
+        
     }
-
-
+    
+    
 }
+
+
+
 
